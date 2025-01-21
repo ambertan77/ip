10ae866c6114f89c3eb2194e3a78895531cb72fe
ambertan <ambertjn@gmail.com>
2025-01-21 18:13:52 +0800

    implement echoing and exit command

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
         String line = "______________________________________________";
         String name = "Bob";
+        String greeting = "Hello! I'm " + name + " :) " + "\nWhat can I do for you?\n" + line;
+        String exit = line + "\n" + "Goodbye, hope to see you again soon! \n" + line;
+
         System.out.println(line);
-        System.out.println("Hello! I'm " + name);
-        System.out.println("What can I do for you?\n" + line);
-        System.out.println("Bye. Hope to see you again soon! \n" + line);
+        System.out.println(greeting);
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(line + "\n" + input + "\n" + line);
+            input = scanner.nextLine();
+        }
+        scanner.close();
+        System.out.println(exit);
     }
 }
