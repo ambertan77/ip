e5a9fed2841da85dfe3e5af1f6cb8ffd18ee11cf
ambertan <ambertjn@gmail.com>
2025-01-22 00:06:57 +0800

    add support for tracking todos, deadlines and events

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class Bob {
 
     public static void main(String[] args) {
+        String indent = "  ";
         String line = "  ______________________________________________";
         String name = "Bob";
-        String greeting = "  Hello! I'm " + name + " :) " + "\n  Let's add to your to-do list!\n" + line;
+        String greeting = "  Hello! I'm " + name + " :) " + "\n  Let's add to your list!\n" + line;
         String mark = "  Nice! I've marked this task as done:\n";
         String unmark = "  OK, I've marked this task as not done yet:\n";
+        String add = "  Got it. I've added this task:";
         String exit = line + "\n" + "  Goodbye, hope to see you again soon! \n" + line;
         Task[] tasks = new Task[100];
         int i = 0;
@@ -16,6 +18,7 @@ public class Bob {
         System.out.println(greeting);
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+
         while (!input.equals("bye")) {
             if (input.equals("list")) {
                 System.out.println(line);
@@ -38,16 +41,32 @@ public class Bob {
                 Task task = tasks[index];
                 task.markAsUndone();
                 System.out.println(unmark + "   " + task.toString() + "\n" + line);
-            }else {
+            } else {
                 if (i >= 100) {
                     System.out.println("List is full.");
                 }
-                tasks[i] = new Task(input);
+                if (input.startsWith("todo ")) {
+                    tasks[i] = new Todos(input.substring(5));
+                } else if (input.startsWith("deadline ")) {
+                    String[] split = input.split(" /");
+                    String desc = split[0].substring(9);
+                    String deadline = split[1].substring(3);
+                    tasks[i] = new Deadline(desc, deadline);
+                } else if (input.startsWith("event ")) {
+                    String[] split = input.split(" /");
+                    String desc = split[0].substring(6);
+                    String from = split[1].substring(5);
+                    String to = split[2].substring(3);
+                    tasks[i] = new Event(desc, from, to);
+                }
+                System.out.println(line + "\n  added: " + add);
+                System.out.println(indent + " " + tasks[i].toString());
                 i++;
-                System.out.println(line + "\n  added: " + input + "\n" + line);
+                System.out.println(indent + "Now you have " + i + " tasks in the list.\n" + line);
             }
             input = scanner.nextLine();
         }
+
         scanner.close();
         System.out.println(exit);
     }
