addf7f0ccff483ec2773cce3a96b179ff9746b52
ambertan77 <ambertjn@gmail.com>
2025-01-21 15:30:44 +0800

    implement greeting and exit messages

---
+++ src/main/java/Bob.java
@@ -0,0 +1,10 @@
+public class Bob {
+    public static void main(String[] args) {
+        String line = "______________________________________________";
+        String name = "Bob";
+        System.out.println(line);
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?\n" + line);
+        System.out.println("Bye. Hope to see you again soon! \n" + line);
+    }
+}
