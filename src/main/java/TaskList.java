37b500fddf80026962ca4906888ecf1415d0fd3d
ambertan <ambertjn@gmail.com>
2025-01-30 00:51:12 +0800

    Add TaskList class

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    // store the current count of tasks in the list
+    protected int count = 0;
+
+    // store the list of tasks
+    protected ArrayList<Task> tasks;
+
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public Task get(int index) {
+        return tasks.get(index);
+    }
+
+    public void remove(Task index) {
+        tasks.remove(index);
+    }
+
+    public void add(Task newTask) {
+        tasks.add(newTask);
+    }
+}
