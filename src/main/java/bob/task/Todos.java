c391ec84f2ac44c30e99b7b3d9ea5f4715892d7f
ambertan <ambertjn@gmail.com>
2025-01-30 02:41:39 +0800

    Fix typo in code comments

--- src/main/java/bob/task/Todos.java
+++ src/main/java/bob/task/Todos.java
@@ -1,4 +1,6 @@
-package bob.task;// solution below adapted from partial solution provided in course website
+package bob.task;
+
+// solution below adapted from partial solution provided in course website
 // https://nus-cs2103-ay2425s2.github.io/website/schedule/week2/project.html under A-Inheritance
 
 import bob.task.Task;
