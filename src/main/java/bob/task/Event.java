433925b86481b574dea8fc88fca590dfe990c6ac
ambertan <ambertjn@gmail.com>
2025-01-31 01:22:21 +0800

    Event.java: Add JavaDoc comments

--- src/main/java/bob/task/Event.java
+++ src/main/java/bob/task/Event.java
@@ -6,6 +6,10 @@ package bob.task;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Represents a Task with a start and end time. A <code>Event</code> object corresponds to
+ * a Task represented by its description, its start time and its end time.
+ */
 public class Event extends Task {
 
     protected LocalDateTime from;
@@ -17,26 +21,54 @@ public class Event extends Task {
         this.to = to;
     }
 
+    /**
+     * Returns the string representation of the task.
+     * Includes the description, start and end time of the task.
+     *
+     * @return String representation of the task.
+     */
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (from " + this.getFrom() + " to " + this.getTo() + ")";
     }
 
+    /**
+     * Returns the string representation of the type of task.
+     *
+     * @return Letter representation of the type of task.
+     */
     public String getType() {
         return "E";
     }
 
+    /**
+     * Returns the description of the task.
+     *
+     * @return Description of task.
+     */
     public String getDescription() {
         return this.description;
     }
 
     // code adapted from https://www.geeksforgeeks.org/java-time-localdatetime-class-in-java/ (Example 3)
+    /**
+     * Returns a string showing the start time of the event.
+     * The deadline is shown in the format: date/month/year, hour:minute.
+     *
+     * @return Start time of event.
+     */
     public String getFrom() {
         DateTimeFormatter outputStringFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy, HH:mm");
         return this.from.format(outputStringFormat);
     }
 
     // code adapted from https://www.geeksforgeeks.org/java-time-localdatetime-class-in-java/ (Example 3)
+    /**
+     * Returns a string showing the end time of the event.
+     * The deadline is shown in the format: date/month/year, hour:minute.
+     *
+     * @return End time of event.
+     */
     public String getTo() {
         DateTimeFormatter outputStringFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy, HH:mm");
         return this.to.format(outputStringFormat);
