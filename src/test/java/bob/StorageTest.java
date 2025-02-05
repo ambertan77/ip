5a7422d68f9bfa50fdf0addc69131192f9c6937b
ambertan <ambertjn@gmail.com>
2025-02-05 17:28:20 +0800

    Change import order for test files

--- src/test/java/bob/StorageTest.java
+++ src/test/java/bob/StorageTest.java
@@ -1,10 +1,11 @@
 package bob;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import bob.task.Task;
 
